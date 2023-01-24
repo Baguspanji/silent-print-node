19f6dc3b32db1889e99d9280461828be4ec0dd0d
bagusp <bagusp0518@gmail.com>
2023-01-24 11:58:43 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { print } = require("./controller/printController.js");
-
-const assert = require('assert');
+const axios = require("axios");
 
 const app = express();
 const port = 8005;
@@ -10,7 +8,12 @@ const port = 8005;
 app.use(express.json());
 app.use(cors());
 
-app.post("/", print);
+app.post("/", async (req, res) => {
+    const body = req.body;
+    const { data } = await axios.post("http://localhost:8003", body);
+
+    res.json(data);
+});
 
 app.get("/", async (req, res) => {
     res.send("Hello Express");
